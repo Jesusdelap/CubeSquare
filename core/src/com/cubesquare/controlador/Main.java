6af479c288879518957c6f2961c8f9bd08ad4c35
d3gus <diegoieslago@gmail.com>
2020-03-28 13:29:48 +0100

    prueba de cambios de comentario en main

--- core/src/com/cubesquare/controlador/Main.java
+++ core/src/com/cubesquare/controlador/Main.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
-
+//Main
 public class Main extends Game {
 
     private AssetManager manager;
