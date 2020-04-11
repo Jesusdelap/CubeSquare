5c3816f28053bec2ed9b81a3737ec6722a00677b
jesusdelap <jesusdelap@gmail.com>
2020-04-11 11:22:22 +0200

    areglao sql

--- desktop/src/com/cubesquare/desktop/DesktopLauncher.java
+++ desktop/src/com/cubesquare/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ import com.cubesquare.controlador.CubeSquare;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width=1280;
-		config.height=720;
+		config.width=1920;
+		config.height=1080;
 		config.addIcon("cubo16x16.png", Files.FileType.Local);
 		config.addIcon("cubo32x32.png", Files.FileType.Local);
 		new LwjglApplication(new CubeSquare(), config);
